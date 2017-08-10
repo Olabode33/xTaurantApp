b92081a30242c1c0d5823155f5992e04669e261e
Olabode A. Bello <olabode33@gmail.com>
2017-08-10 17:20:08 +0100

    Add files via upload

---
+++ php/SendGrid/index.php
@@ -0,0 +1,36 @@
+<?php
+
+	require("sendgrid-php/sendgrid-php.php");
+	
+	$request_body = json_decode('{
+														  "personalizations": [
+															{
+															  "to": [
+																{
+																  "email": "becheks@gmail.com"
+																}
+															  ],
+															  "subject": "Sending with SendGrid is Fun"
+															}
+														  ],
+														  "from": {
+															"email": "info@xtaurantapp.com"
+														  },
+														  "content": [
+															{
+															  "type": "text/plain",
+															  "value": "and easy to do anywhere, even with PHP"
+															}
+														  ]
+														}');
+	
+	$apiKey = getenv('');
+	$sg = new \SendGrid($apiKey);
+	
+	$response = $sg->client->mail()->send()->post($request_body);
+	
+	echo $response->statusCode();
+	print_r($response->headers());
+	echo $response->body();
+
+?>
