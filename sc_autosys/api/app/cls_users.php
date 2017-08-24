b8d4beac584b00ac7064896b66c9b11389eab6e4
Olabode A. Bello <olabode33@gmail.com>
2017-08-24 14:54:26 +0100

    Update cls_users.php

--- sc_autosys/api/app/cls_users.php
+++ sc_autosys/api/app/cls_users.php
@@ -164,11 +164,14 @@
 			//$uname = isset($_POST['username'])? mysql_real_escape_string($_POST['username']) : '';
 			//$pass = isset($_POST['password'])? mysql_real_escape_string($_POST['password']) : '';
 			
-			$user = isset($_POST['username'])? filter_input(INPUT_POST, $_POST['username']) : '';
-			$pass = isset($_POST['password'])? filter_input(INPUT_POST, $_POST['password']) : '';
+			//$user = isset($_POST['username'])? filter_input(INPUT_POST, $_POST['username']) : '';
+			//$pass = isset($_POST['password'])? filter_input(INPUT_POST, $_POST['password']) : '';
+			
+			$user = $_POST['username'];
+			$pass = $_POST['password'];
 			
 			$users = $this->get_user();
-			$msg = array("user" => $_POST['username'], "pass" => $pass);
+			//$msg = array("user" => $_POST['username'], "pass" => $_POST['password']);
 			
 			//print_r($users);
 			//echo '<br>'.$uname;
@@ -185,12 +188,13 @@
 					$_SESSION['sc_branch'] = $user['branch'];
 					$_SESSION['sc_status'] = $user['status'];
 					
-					//$msg = array("status" => 1, "msg" => "Logged in successfully");
+					$msg = array("status" => 1, "msg" => "Logged in successfully");
 					//$msg = $_POST;
-					return $msg;
+					//return $msg;
+					break;
 				}
 				else {
-					//$msg = array("status" => 0, "msg" => "Username not found!");
+					$msg = array("status" => 0, "msg" => "Username not found!");
 				}
 			}
 			
