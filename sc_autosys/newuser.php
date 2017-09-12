bd8c6b141f6f656de12d1fddfeba0310757355a1
Olabode A. Bello <olabode33@gmail.com>
2017-09-12 17:44:22 +0100

    Add files via upload

---
+++ sc_autosys/newuser.php
@@ -0,0 +1,216 @@
+	<?php
+		include "includes/header_pages.php";
+	?>
+		
+	<div>
+		<ul class="nav nav-pills" style="margin-top: 10px; margin-bottom: 10px;">
+			<li role="presentation" class="active"><a href="newuser.php"><i class="fa fa-user-plus"></i> New</a></li>
+			<!--li role="presentation"><a href="users.php">Existing</a></li-->
+		</ul>
+	</div>
+	<hr  style="margin-top: 10px; margin-bottom: 10px;">
+
+	<div class="row" style="margin-top:20px">
+		<div class="col-xs-12 " id="insert_cus">
+			<div class="alert alert-info alert-dismissible hidden" role="alert" id="insert_alert">
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>	
+				<i class="fa fa-info-circle"></i> 
+				<span id="insert_msg"></span>
+			</div>
+		</div>
+		<form action="#" class="form-horizontal" id="newuser_form">
+			<div class="col-md-10">		
+				<div class="form-group">
+					<label for="cardno" class="col-sm-2 control-label">Title</label>
+					<div class="col-sm-4">
+						<input type="text" id="title" name="title" class="form-control" placeholder="Enter Title">
+					</div>
+				</div>
+								
+				<div class="form-group">
+					<label for="sname" class="col-sm-2 control-label"> <span class="text-danger">*</span>Surname</label>
+					<div class="col-sm-4">
+						<input type="text" id="sname" name="sname" class="form-control" placeholder="Your surname..." required>
+					</div>
+								
+					<label for="fname" class="col-sm-2 control-label">Firstname</label>
+					<div class="col-sm-4">
+						<input type="text" id="fname" name="fname" class="form-control" placeholder="Your firstname...">
+					</div>	
+				</div>
+								
+				<div class="form-group">	
+					<label for="uname" class="col-sm-2 control-label">Username</label>
+					<div class="col-sm-4">
+						<input type="text" id="uname" name="uname" class="form-control" placeholder="Your username / login name...">
+					</div>
+				</div>
+				
+				<div class="form-group">	
+					<label for="role" class="col-sm-2 control-label">Role</label>
+					<div class="col-sm-4">
+						<select class="form-control" Required id="role" name="role">
+							<option value="">-- Select a role --</option>
+							<option value="Doctor">Doctor</option>
+							<option value="Receptionist">Receptionist</option>
+							<option value="Admin">Admin</option>
+						</select>
+					</div>
+				</div>
+								
+				<div class="form-group" id="pass_grp">								
+					<label for="pass" class="col-sm-2 control-label">Password</label>
+					<div class="col-sm-4">
+						<input type="password" id="pass" name="pass" class="form-control" placeholder="Enter your password..." required>
+					</div>
+					
+					<label for="cpass" class="col-sm-2 control-label">Confirm Password</label>
+					<div class="col-sm-4">
+						<input type="password" id="cpass" name="cpass" class="form-control" placeholder="Confirm your password..." required>
+					</div>
+				</div>
+							
+				<div class="form-group">	
+					<label for="ubranch" class="col-sm-2 control-label">Branch</label>
+					<div class="col-sm-4">
+						<select class="form-control" id="ubranch" name="ubranch">
+							<option value="">-- Select a branch --</option>
+							<option value="VI">VI</option>
+							<option value="Ikeja">Ikeja</option>
+						</select>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-2">
+				<a class="btn btn-block btn-default" style="text-align: left;" id="btn_back" href="users.php">
+					<i class="fa fa-reply" id=""></i> Back to list
+				</a> 
+				<button type="reset" class="btn btn-block btn-default" style="text-align: left;" id="btn_clear">
+					<i class="fa fa-eraser" id=""></i> Clear
+				</button> 
+				<button type="submit" style="color:white; text-align: left;" class="btn btn-block sctheme">
+					<i class="fa fa-save" id="save_icon"></i> Save
+				</button> 
+			</div>
+							
+			<div style="margin-bottom:3%;"> </div>
+		</form>
+	</div>
+
+	<script>
+		$(document).ready( function() {		
+			//$("#rship_acc").select2();
+			
+			$("#pass").keyup(function () {
+				validate();
+			});
+			$("#cpass").keyup(function () {
+				validate();
+			});
+			
+			var id = location.search;	
+			id = id.split("=");
+			id = id[1];
+			if(id != undefined){			
+				//$("#frm_title").html("Edit User")
+				$.get('api/Users_RestController.php?view=single&id='+id, function(data) {
+					data = $.parseJSON(data);
+					//console.log(data);
+					if(data.status === undefined){
+						$.each(data, function(i, item) {			
+							$("#title").val(item.title);
+							$("#sname").val(item.lname);
+							$("#fname").val(item.fname);
+							$("#role").val(item.role).change();
+							$("#uname").val(item.uname);	
+							$("#ubranch").val(item.branch).change();
+							
+							$("#btn_clear").addClass("hidden");
+						});
+					}
+				});
+			}
+		
+			$("#newuser_form").submit(function(e) { 
+				$("#save_icon").removeClass("fa-save");
+				$("#save_icon").addClass("fa-spinner fa-pulse");
+				
+				e.preventDefault();
+				
+				if(validate()){
+					$.ajax({
+						url: (id != undefined) ?  'api/Users_RestController.php?view=edit&id=' + id : 'api/Users_RestController.php?view=new',
+						type: 'post',
+						data : {
+							'title' : $("#title").val(),
+							'lname' : $("#sname").val(),
+							'fname' :  $("#fname").val(),
+							'role': $("#role").val(),
+							'uname': $("#uname").val(),
+							'pass': $("#pass").val(),
+							'branch': $("#ubranch").val()
+						},
+						success: function(data) {
+						   //alert (data);
+						   data = $.parseJSON(data);
+						   if(data.status == 1){
+								$("#save_icon").removeClass("fa-spinner fa-pulse");
+								$("#save_icon").addClass("fa-save");
+								$("#insert_alert").removeClass("alert-info");
+								$("#insert_alert").removeClass("alert-warning");
+								$("#insert_alert").addClass("alert-success");
+								$("#insert_msg").html(data.msg);
+								$("#insert_alert").removeClass("hidden");
+								$("#newuser_form").trigger("reset");
+						   }
+						   else {
+								$("#save_icon").removeClass("fa-spinner fa-pulse");
+								$("#save_icon").addClass("fa-save");
+								$("#insert_alert").removeClass("alert-info");
+								$("#insert_alert").removeClass("alert-warning");
+								$("#insert_alert").addClass("alert-danger");
+								$("#insert_msg").html(data.msg);
+								$("#insert_cus").removeClass("hidden");
+						   }
+						}
+					});
+				}
+				else {
+					$("#save_icon").removeClass("fa-spinner fa-pulse");
+					$("#save_icon").addClass("fa-save");
+					$("#insert_alert").removeClass("alert-info");
+					$("#insert_alert").addClass("alert-warning");
+					$("#insert_msg").html("Password mis-match, Please retype the password");
+					$("#insert_alert").removeClass("hidden");
+				}
+			});
+			
+			function validate() {
+				var pass = $('#pass').val();
+				var cpass = $('#cpass').val();
+				
+				if(pass != "" || cpass != ""){
+					if(pass == cpass){
+						$("#pass_grp").removeClass("has-error");
+						$("#pass_grp").addClass("has-success");
+						return true;
+					}
+					else {
+						$("#pass_grp").removeClass("has-success")
+						$("#pass_grp").addClass("has-error");
+						return false;
+					}
+				}
+				else {
+					$("#pass_grp").removeClass("has-error");
+					$("#pass_grp").addClass("has-error");
+					return false;
+				}
+			}
+		});
+	</script>
+	
+	<?php
+		include "includes/footer_pages.php";
+	?>
---
+++ sc_autosys/users.php
@@ -0,0 +1,202 @@
+	<?php
+		include "includes/header_pages.php";
+	?>
+	
+	<div>
+		<ul class="nav nav-pills" style="margin-top:1%">
+			<li role="presentation"><a href="newuser.php"><i class="fa fa-user-plus"></i> New</a></li>
+			<!--li role="presentation" class="active"><a href="users.php">Existing</a></li-->
+		</ul>
+	</div>
+	<hr  style="margin-top: 10px; margin-bottom: 10px;">
+
+	<div class="row" style="margin-top: 20px">
+		<div class="col-xs-12 col-sm-6 hidden" style="margin-bottom: 20px">
+			<div class="input-group">
+				<div class="input-group-addon"><i class="fa fa-search"></i></div>
+				<input type="text" id="txt_search" name="txt_search" class="form-control input-lg" placeholder="Enter your search terms">
+			</div>
+		</div>
+		<div class="col-xs-12 hidden" id="loading_cus">
+			<div class="alert alert-info" role="alert">
+				<i class="fa fa-search"></i> <span id="loading_txt">Showing top 10 results</span>
+			</div>
+		</div>
+		<div class="col-xs-12">
+			<div id="summary-list">
+				<div class="table-responsive">
+					<table class="table table-hover" id="tbl_users" >
+						<thead class="thead-inverse">	
+							<th class="col-xs-2" >Role</th>						
+							<th class="col-xs-2">Surname</th>
+							<th class="col-xs-2">First Name</th>
+							<th class="col-xs-2">Username</th>	
+							<th class="col-xs-2">Branch</th>
+							<th class="col-xs-2">Actions</th>				
+						</thead>
+						
+						<tbody>
+							
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<div class="hidden" id="details">
+				<div class="row" style="margin-bottom: 10px;">
+					<div class="col-sm-10">
+						<div class="table-responsive">
+							<table class="table table-striped">
+								<tr>
+									<td class="col-sm-2"><label><strong>Card No.</strong></label></td>
+									<td class="col-sm-4" id="cardno">_____________</td>		
+									<td class="col-sm-2"><label><strong>Enrolee ID</strong></label></td>
+									<td class="col-sm-4" id="enroleeno">_____________</td>	
+								</tr>
+								<tr>
+									<td class="col-sm-2"><label><strong>Full Name</strong></label></td>
+									<td class="col-sm-4" id="fullname">_____________</td>		
+									<td class="col-sm-2"><label><strong>Gender</strong></label></td>
+									<td class="col-sm-4" id="gender">_____________</td>	
+								</tr>
+								<tr>
+									<td><label><strong>Date of Birth</strong></label></td>
+									<td id="dob">_____________</td>		
+									<td><label><strong>Age</strong></label></td>
+									<td id="age">_____________</td>	
+								</tr>
+								<tr>
+									<td><label><strong>Address</strong></label></td>
+									<td colspan="3" id="fulladdress">_____________</td>
+								</tr>
+								<tr>
+									<td><label><strong>Occupation</strong></label></td>
+									<td id="occupation">_____________</td>		
+									<td><label><strong>Next of Kin</strong></label></td>
+									<td id="nok">_____________</td>	
+								</tr>
+								<tr>
+									<td><label><strong>Mobile</strong></label></td>
+									<td id="mobile">_____________</td>		
+									<td><label><strong>Email</strong></label></td>
+									<td id="email">_____________</td>	
+								</tr>
+								<tr>
+									<td><label><strong>Relationship</strong></label></td>
+									<td id="rship_type">_____________</td>		
+									<td><label><strong>Account</strong></label></td>
+									<td id="rship_account">_____________</td>	
+								</tr>
+							</table>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<button class="btn btn-default btn-block" onclick="view_summary()" style="text-align: left;"><i class="fa fa-reply"></i> Back To Summary</button>
+						<a style="color:white; text-align: left;" class="btn btn-block sctheme btn-block" id="btn_udpated" href="#"><i class="fa fa-pencil-square-o"></i> Update</a> 
+						<a style="color:white; background-color:cornflowerblue; text-align: left;" class="btn btn-block" id="btn_book_app" href="#"><i class="fa fa-calendar-plus-o"></i> Book Appointment</a> 
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<script>
+		$(document).ready( function() {
+			//$('#loading_cus').removeClass("hidden");
+			$("#summary-list").addClass("hidden");
+			search();
+			
+			$("#txt_search").keyup(function () {
+				search();
+			});
+		});
+		
+		function search () {
+			var term = $("#txt_search").val();
+			
+			$.get('api/Users_RestController.php?view=all', function(data) {
+				data = $.parseJSON(data);
+				//console.log(data);
+					
+				var t = $("#tbl_users");
+				$("#tbl_users tbody").empty();
+					
+				if(data.status != 0) {
+					$("#loading_txt").html("Showing top " + data.length + " results!");
+					
+					$.each(data, function(i, item) {
+						row = $('<tr></tr>');
+						rowData = $('<td></td>').text(item.role);
+						row.append(rowData);
+						rowData = $('<td></td>').text(item.lname);
+						row.append(rowData);
+						rowData = $('<td></td>').text(item.fname);
+						row.append(rowData);
+						rowData = $('<td></td>').text(item.uname);
+						row.append(rowData);
+						rowData = $('<td></td>').text(item.branch);
+						row.append(rowData);
+							
+						viewButton = $('<a></a>').addClass("btn btn-warning btn-xs details").text("Edit");
+						viewButton.attr('href', 'newuser.php?id='+item.userid);
+						viewButton.attr('data-toggle', 'tooltip');
+						viewButton.attr('title', 'View Details');
+						//viewButton.attr('', 'view_details('+item.cid+')');
+						rowData = $('<td></td>').append(viewButton);
+							
+						row.append(rowData);
+							
+						t.append(row);
+					});
+				}
+				else {
+					$("#loading_cus_txt").html(data.msg);
+				}
+					
+				$("#summary-list").removeClass("hidden");
+				$("#loading_cus").removeClass("hidden");
+			});
+		}
+		
+		function view_details(id) {
+			//alert("button " +id+" was clicked");
+			$("#summary-list").addClass("hidden");
+			$("#details").removeClass("hidden");
+			$("#loading_cus").addClass("hidden");
+			
+			$.get('api/Customers_RestController.php?view=single&id='+id, function(data) {
+				data = $.parseJSON(data);
+				console.log(data);
+				
+				$.each(data, function(i, item) {
+					$("#cardno").html(item.cardno);
+					$("#enroleeno").html(item.enroleeid);
+					$("#fullname").html(item.title + " " + item.fname + ' ' + item.lname);
+					$("#gender").html(item.gender);
+					$("#fulladdress").html(item.address + ", " + item.address_area + " " + item.address_state);
+					$("#dob").html(item.dob);
+					$("#age").html(item.age);
+					$("#occupation").html(item.occupation);
+					$("#nok").html(item.nok);
+					$("#mobile").html(item.phone1 + ", " + item.phone2);
+					$("#email").html(item.email1 + ",  " + item.email2);
+					$("#rship_type").html(item.rship_type);
+					$("#rship_account").html(item.rship_account);
+					$("#btn_udpated").attr("href", "newcust.php?id="+item.cid);
+					$("#btn_book_app").attr("href", "newcust.php?id="+item.cid);
+				});
+			});
+		}
+		
+		function view_summary() {
+			$("#summary-list").removeClass("hidden");
+			$("#details").addClass("hidden");
+		}
+		
+		function update_record() {
+			
+		}
+		
+	</script>	
+	<?php
+		include "includes/footer_pages.php";
+	?>
