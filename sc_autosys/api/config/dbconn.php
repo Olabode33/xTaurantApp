6ee51d8d025ce7306428651685247211a87c7867
Olabode A. Bello <olabode33@gmail.com>
2017-11-01 17:30:09 +0100

    Update dbconn.php

--- sc_autosys/api/config/dbconn.php
+++ sc_autosys/api/config/dbconn.php
@@ -3,7 +3,7 @@
 		function __construct(){}
 
 		//function db_connect($user='root', $password='P@ssw0rd', $db='sc_autosys_2')
-		function db_connect($user='root', $password="", $db="sc_autosys_2")
+		function db_connect($user='sc_db_user', $password="HQ2dHZscf7KrncpZ", $db="sc_autosys_2")
 		{
 			//root P@ssw0rd
 			$conn = mysqli_connect('127.0.0.1', $user, $password);
