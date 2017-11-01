db0cb027e788b622da68706ba6633ad45e041e70
Olabode A. Bello <olabode33@gmail.com>
2017-11-01 17:15:48 +0100

    Delete menu-navbar.php

--- includes/menu-navbar.php
+++
@@ -1,9 +0,0 @@
-	<div class="navbar navbar-inverse menu-wrap">
-      <div class="navbar-header text-center">
-			<h3>Your Orders</h3>
-      </div>
-		<div id="orderedItems">
-			<?php include "pages/mini-includes/current_order_list.php"; ?>
-		</div>
-        <button class="close-button" id="close-button">Close Menu</button>
-    </div>
