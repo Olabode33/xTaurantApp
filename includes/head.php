44881585aaf50836684e740d060be69b25f7ca20
Olabode A. Bello <olabode33@gmail.com>
2017-11-02 11:17:56 +0100

    fix for xtaurantapp

--- includes/head.php
+++ includes/head.php
@@ -1,13 +1,18 @@
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-	
-		<title>.:SC AutoSys::.</title>
+  <head>  	
+  	<meta charset="utf-8">
+    <meta name="description" content="Restaurant Management Application">
+    <meta name="keywords" content="Restaurant">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="author" content="Olabode A. Bello">
+		<title>xTaurantApp</title>
 
-		<!-- Bootstrap -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/sctheme.css" rel="stylesheet">
-		<link rel="stylesheet" href="fonts/font-awesome.min.css" type="text/css" media="screen">
-		<script src="js/jquery-2.1.4.min.js"></script>
-	</head>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="fonts/font-awesome.min.css" type="text/css" media="screen">
+    <!-- Include roboto.css to use the Roboto web font, material.css to include the theme and ripples.css to style the ripple effect -->
+    <link href="css/material.min.css" rel="stylesheet">
+    <link href="css/ripples.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+  </head>
